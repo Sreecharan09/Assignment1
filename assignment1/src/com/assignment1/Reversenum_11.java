1ca193ae077949c60dd81aac6585bf40b6099168
koppl <koppl@CHARAN>
2024-06-26 16:17:44 +0530

    java basic programs

---
+++ assignment1/src/com/assignment1/Reversenum_11.java
@@ -0,0 +1,22 @@
+
+package com.assignment1;
+public class Reversenum_11{
+    public static void Reverse(int num)
+   {
+
+        if (num < 10) {
+              System.out.println(num);
+              return;
+       }
+       else {
+              System.out.print(num % 10);
+              Reverse(num / 10);
+       }
+    }
+    public static void main(String args[])
+    { 
+       int num = 98765;
+       System.out.print("Reversed Number: ");
+       Reverse(num);
+}
+}
---
+++ assignment1/src/com/assignment1/greatestNumber_4.java
@@ -0,0 +1,22 @@
+package com.assignment1;
+
+import java.util.Scanner;
+
+public class greatestNumber_4 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+	    System.out.println("Enter a first number:");
+		int n1=sc.nextInt();
+	    System.out.println("Enter a second number:");
+		int n2=sc.nextInt();
+		if(n1>n2) {
+			System.out.println(n1+ " is greater than " +n2);
+		}
+		else {
+			System.out.println(n2+ " is greater than " +n1);
+		}
+
+	}
+
+}
